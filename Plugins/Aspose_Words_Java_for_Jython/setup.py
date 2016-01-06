5a2413b7d4adeba412ae6f28f5043cd4220c5bd1
Masood Anwer <masood.anwer@aspose.com>
2016-01-06 15:29:49 +0500

    Update setup.py

--- Plugins/Aspose_Words_Java_for_Jython/setup.py
+++ Plugins/Aspose_Words_Java_for_Jython/setup.py
@@ -6,10 +6,10 @@ setup(
     name = 'asposewordsjavajython',
     packages = find_packages(),
     version = '1.0.0',
-    description = 'Aspose.Words for Jython allows you to use Aspose.Words for Java API in your Jython applications',
+    description = 'Aspose.Words Java for Jython allows you to use Aspose.Words for Java API in your Jython applications',
     author='Masood Anwer',
     author_email='masood.anwer@aspose.com',
-    url='',
+    url='https://github.com/asposewords/Aspose_Words_Java/tree/master/Plugins/Aspose_Words_Java_for_Jython',
     classifiers=[
         'Programming Language :: Jython',
         'Programming Language :: Jython :: 2',
