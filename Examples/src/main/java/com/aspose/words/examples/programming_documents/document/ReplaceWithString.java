960d87ef0faef6c3548602570875bb4a8d7cd04d
aspose-words <words@aspose.com>
2016-09-08 11:15:32 +0500

    FindAndHighlightText Example

--- Examples/src/main/java/com/aspose/words/examples/programming_documents/document/ReplaceWithString.java
+++ Examples/src/main/java/com/aspose/words/examples/programming_documents/document/ReplaceWithString.java
@@ -3,17 +3,14 @@ package com.aspose.words.examples.programming_documents.document;
 import com.aspose.words.Document;
 import com.aspose.words.examples.Utils;
 
-
 public class ReplaceWithString {
-    public static void main(String[] args) throws Exception {
-        //ExStart:1
+    @SuppressWarnings("deprecation")
+	public static void main(String[] args) throws Exception {
         // The path to the documents directory.
         String dataDir = Utils.getDataDir(ReplaceWithString.class);
 
         Document doc = new Document(dataDir + "Document.doc");
         doc.getRange().replace("sad", "bad", false, true);
-        doc.save(dataDir + "output.doc");
-        //ExEnd:1
+        doc.save(dataDir + "output.doc");   
     }
-
 }
