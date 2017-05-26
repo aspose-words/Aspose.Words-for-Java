445924b7a0d269417550ab3b28131bb8113c7d58
Aspose.Words Product Family <words@aspose.com>
2017-05-26 15:31:36 +0500

    New FindReplaceOptions passed to Replace method

--- Examples/src/main/java/com/aspose/words/examples/programming_documents/tables/ExtractOrReplaceText/ReplaceText.java
+++ Examples/src/main/java/com/aspose/words/examples/programming_documents/tables/ExtractOrReplaceText/ReplaceText.java
@@ -1,8 +1,6 @@
 package com.aspose.words.examples.programming_documents.tables.ExtractOrReplaceText;
 
-import com.aspose.words.Document;
-import com.aspose.words.NodeType;
-import com.aspose.words.Table;
+import com.aspose.words.*;
 import com.aspose.words.examples.Utils;
 
 public class ReplaceText {
@@ -15,10 +13,15 @@ public class ReplaceText {
 		// Get the first table in the document.
 		Table table = (Table) doc.getChild(NodeType.TABLE, 0, true);
 
+		FindReplaceOptions opts = new FindReplaceOptions();
+		opts.MatchCase = true;
+		opts.FindWholeWordsOnly = true;
+
 		// Replace any instances of our string in the entire table.
-		table.getRange().replace("Carrots", "Eggs", true, true);
+		table.getRange().replace("Carrots", "Eggs", opts);
+
 		// Replace any instances of our string in the last cell of the table only.
-		table.getLastRow().getLastCell().getRange().replace("50", "20", true, true);
+		table.getLastRow().getLastCell().getRange().replace("50", "20", opts);
 
 		doc.save(dataDir + "Table.ReplaceCellText Out.docx");
 	}
