ad3424813e05a5a852eb4ecb50ab9d623c31b2ea
Tilal Ahmad Sana <tilal.ahmad@aspose.com>
2017-05-29 12:40:47 +0500

    Aspose.17.5 Fields Examples
    Apply Custom Field Formatting - Insert INCLUDETEXT Field - Insert TOA
    Field

---
+++ Examples/src/main/java/com/aspose/words/examples/programming_documents/fields/FieldResultFormatter.java
@@ -0,0 +1,54 @@
+package com.aspose.words.examples.programming_documents.fields;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import com.aspose.words.IFieldResultFormatter;
+/**
+ * Created by Home on 5/29/2017.
+ */
+public class FieldResultFormatter implements IFieldResultFormatter {
+
+    private final String mNumberFormat;
+    private final String mDateFormat;
+
+    private final ArrayList mNumberFormatInvocations = new ArrayList();
+    private final ArrayList mDateFormatInvocations = new ArrayList();
+
+    public FieldResultFormatter(String numberFormat, String dateFormat) {
+        mNumberFormat = numberFormat;
+        mDateFormat = dateFormat;
+    }
+
+    public FieldResultFormatter() {
+        mNumberFormat = null;
+        mDateFormat = null;
+    }
+
+    public String format(String arg0, int arg1) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String format(double arg0, int arg1) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String formatNumeric(double value, String format) {
+        // TODO Auto-generated method stub
+
+        mNumberFormatInvocations.add(new Object[] { value, format });
+        return (mNumberFormat.isEmpty() || mNumberFormat == null) ? null
+                : String.format(mNumberFormat, value);
+    }
+
+    public String formatDateTime(Date value, String format, int calendarType) {
+        mDateFormatInvocations
+                .add(new Object[] { value, format, calendarType });
+
+        return (mDateFormat.isEmpty() || mDateFormat == null) ? null : String
+                .format(mDateFormat, value);
+    }
+
+}
