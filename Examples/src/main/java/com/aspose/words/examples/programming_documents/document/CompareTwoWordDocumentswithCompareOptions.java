25b77dc9f7f0f495a2d2a2114d84b72a2fed4daa
Tilal Ahmad Sana <tilal.ahmad@aspose.com>
2017-05-29 14:39:47 +0500

    Aspose.Words 17.5 examples
    ApplyCellPadding-BindingContentControlwithXML-SaveImagewithResolution-CompareDocumentwithOptions

---
+++ Examples/src/main/java/com/aspose/words/examples/programming_documents/document/CompareTwoWordDocumentswithCompareOptions.java
@@ -0,0 +1,33 @@
+package com.aspose.words.examples.programming_documents.document;
+
+import java.util.Date;
+
+import com.aspose.words.Document;
+import com.aspose.words.examples.Utils;
+
+
+
+/**
+ * Created by Home on 5/29/2017.
+ */
+public class CompareTwoWordDocumentswithCompareOptions {
+
+
+    public static void main(String[] args) throws Exception {
+
+        String dataDir = Utils.getDataDir(CompareTwoWordDocumentswithCompareOptions.class);
+
+        com.aspose.words.Document docA = new com.aspose.words.Document(dataDir + "DocumentA.doc");
+        com.aspose.words.Document docB = new com.aspose.words.Document(dataDir + "DocumentB.doc");
+
+        com.aspose.words.CompareOptions options = new com.aspose.words.CompareOptions();
+        options.setIgnoreFormatting(true);
+        options.setIgnoreHeadersAndFooters(true);
+        // DocA now contains changes as revisions.
+        docA.compare(docB, "user", new Date(), options);
+        if (docA.getRevisions().getCount() == 0)
+            System.out.println("Documents are equal");
+        else
+            System.out.println("Documents are not equal");
+    }
+}
---
+++ Examples/src/main/java/com/aspose/words/examples/rendering_printing/SaveImageWithResolution.java
@@ -0,0 +1,24 @@
+package com.aspose.words.examples.rendering_printing;
+
+import com.aspose.words.*;
+import com.aspose.words.examples.Utils;
+
+/**
+ * Created by Home on 5/29/2017.
+ */
+public class SaveImageWithResolution {
+    public static void main(String[] args) throws Exception {
+
+        // The path to the documents directory.
+        String dataDir = Utils.getDataDir(SaveImageWithResolution.class);
+        com.aspose.words.Document doc = new com.aspose.words.Document(dataDir + "TestFile.doc");
+
+        //Renders a page of a Word document into a PNG image at a specific horizontal and vertical resolution.
+        ImageSaveOptions options = new ImageSaveOptions(com.aspose.words.SaveFormat.PNG);
+        options.setHorizontalResolution(300);
+        options.setVerticalResolution(300);
+        options.setPageCount(1);
+
+        doc.save(dataDir + "Rendering.SaveToImageResolution Out.png", options);
+    }
+}
