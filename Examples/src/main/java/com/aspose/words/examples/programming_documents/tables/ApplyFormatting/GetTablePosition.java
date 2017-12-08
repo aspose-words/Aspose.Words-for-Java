af60278e22e8f528787165e4c0d25518d4e3db34
Aspose.Words Product Family <words@aspose.com>
2017-12-08 11:06:49 +0500

    Examples 17.11
    Examples for Aspose.Words for Java 17.11 have been added

--- Examples/src/main/java/com/aspose/words/examples/programming_documents/tables/ApplyFormatting/GetTablePosition.java
+++ Examples/src/main/java/com/aspose/words/examples/programming_documents/tables/ApplyFormatting/GetTablePosition.java
@@ -14,23 +14,45 @@ public class GetTablePosition {
     public static void main(String[] args) throws Exception {
         // The path to the documents directory.
         String dataDir = Utils.getDataDir(GetTablePosition.class);
+
+        getTablePosition(dataDir);
+        getFloatingTablePosition(dataDir);
+    }
+
+    private static void getTablePosition(String dataDir) throws Exception {
         // ExStart:GetTablePosition
         Document doc = new Document(dataDir + "Table.Document.doc");
 
         // Retrieve the first table in the document.
-        Table table = (Table)doc.getChild(NodeType.TABLE, 0, true);
+        Table table = (Table) doc.getChild(NodeType.TABLE, 0, true);
 
-        if (table.getTextWrapping() == TextWrapping.AROUND)
-        {
-            System.out.println("RelativeHorizontalAlignment: "+table.getRelativeHorizontalAlignment());
-            System.out.println("RelativeVerticalAlignment: "+table.getRelativeVerticalAlignment());
-        }
-        else
-        {
-            System.out.println("Alignment: "+table.getAlignment());
+        if (table.getTextWrapping() == TextWrapping.AROUND) {
+            System.out.println("RelativeHorizontalAlignment: " + table.getRelativeHorizontalAlignment());
+            System.out.println("RelativeVerticalAlignment: " + table.getRelativeVerticalAlignment());
+        } else {
+            System.out.println("Alignment: " + table.getAlignment());
         }
 
         // ExEnd:GetTablePosition
+        System.out.println("\nTable position fetched successfully.");
+    }
+
+    private static void getFloatingTablePosition(String dataDir) throws Exception {
+        // ExStart:GetFloatingTablePosition
+        Document doc = new Document(dataDir + "FloatingTablePosition.docx");
+        for (Table table : doc.getFirstSection().getBody().getTables()) {
+            // If table is floating type then print its positioning properties.
+            if (table.getTextWrapping() == TextWrapping.AROUND) {
+                System.out.println(table.getHorizontalAnchor());
+                System.out.println(table.getVerticalAnchor());
+                System.out.println(table.getAbsoluteHorizontalDistance());
+                System.out.println(table.getAbsoluteVerticalDistance());
+                System.out.println(table.getAllowOverlap());
+                System.out.println("..............................");
+            }
+        }
+
+        // ExEnd:GetFloatingTablePosition
         System.out.println("\nTable position fetched successfully.");
     }
 }
