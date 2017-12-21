6f42ce8eb177ceb0eda788eeba566727a23314fe
Aspose.Words Product Family <words@aspose.com>
2017-12-21 15:08:04 +0500

    Apply Metered License
    How to Apply Metered License example added.

--- Examples/src/main/java/com/aspose/words/examples/quickstart/ApplyLicense.java
+++ Examples/src/main/java/com/aspose/words/examples/quickstart/ApplyLicense.java
@@ -1,28 +1,45 @@
 
-  
+
 package com.aspose.words.examples.quickstart;
 
-import com.aspose.words.License;
+import com.aspose.words.*;
 
-public class ApplyLicense
-{
-    public static void main(String[] args) throws Exception
-    {
+public class ApplyLicense {
+    public static void main(String[] args) throws Exception {
         //ExStart:
         // This line attempts to set a license from several locations relative to the executable and Aspose.Words.dll.
         // You can also use the additional overload to load a license from a stream, this is useful for instance when the
         // license is stored as an embedded resource
-        try
-        {
+        try {
             License license = new License();
             license.setLicense("Aspose.Words.lic");
             System.out.println("License set successfully.");
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             // We do not ship any license with this example, visit the Aspose site to obtain either a temporary or permanent license.
             System.out.println("There was an error setting the license: " + e.getMessage());
         }
         //ExEnd:
+    }
+
+    public static void ApplyMeteredLicense() throws Exception {
+        //ExStart:ApplyMeteredLicense
+        String publicKey = "";
+        String privateKey = "";
+
+        Metered m = new Metered();
+        m.setMeteredKey(publicKey, privateKey);
+
+        // Optionally, the following two lines returns true if a valid license has been applied;
+        // false if the component is running in evaluation mode.
+        License lic = new License();
+        System.out.println("License is set = " + lic.isLicensed());
+
+        Document doc = new Document();
+        DocumentBuilder builder = new DocumentBuilder(doc);
+
+        builder.write("Hello World!");
+
+        System.out.println(doc.toString(SaveFormat.TEXT));
+        //ExEnd:ApplyMeteredLicense
     }
 }
