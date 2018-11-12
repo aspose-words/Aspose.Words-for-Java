39b48487f05c8316a65ab91492da4598c84bc6c1
Aspose.Words Product Family <words@aspose.com>
2018-11-12 12:12:12 +0500

    Examples 18.11
    Examples for Aspose.Words for Java 18.11 have been added.

--- Examples/src/main/java/com/aspose/words/examples/loading_saving/WorkingWithTxt.java
+++ Examples/src/main/java/com/aspose/words/examples/loading_saving/WorkingWithTxt.java
@@ -11,6 +11,7 @@ public class WorkingWithTxt {
         addBidiMarks(dataDir);
         detectNumberingWithWhitespaces(dataDir);
         handleSpacesOptions(dataDir);
+        exportHeadersFootersMode(dataDir);
     }
 
     public static void saveAsTxt(String dataDir) throws Exception {
@@ -59,5 +60,29 @@ public class WorkingWithTxt {
         doc.save(dataDir);
         //ExEnd:HandleSpacesOptions
         System.out.println("\nTrim leading and trailing spaces while importing text document.\nFile saved at " + dataDir);
+    }
+
+    public static void exportHeadersFootersMode(String dataDir) throws Exception {
+        //ExStart:ExportHeadersFootersMode
+
+        Document doc = new Document(dataDir + "TxtExportHeadersFootersMode.docx");
+
+        TxtSaveOptions options = new TxtSaveOptions();
+        options.setSaveFormat(SaveFormat.TEXT);
+
+        // All headers and footers are placed at the very end of the output document.
+        options.setExportHeadersFootersMode(TxtExportHeadersFootersMode.ALL_AT_END);
+        doc.save(dataDir + "outputFileNameA.txt", options);
+
+        // Only primary headers and footers are exported at the beginning and end of each section.
+        options.setExportHeadersFootersMode(TxtExportHeadersFootersMode.PRIMARY_ONLY);
+        doc.save(dataDir + "outputFileNameB.txt", options);
+
+        // No headers and footers are exported.
+        options.setExportHeadersFootersMode(TxtExportHeadersFootersMode.NONE);
+        doc.save(dataDir + "outputFileNameC.txt", options);
+
+        //ExEnd:ExportHeadersFootersMode
+        System.out.println("\nExport text files with TxtExportHeadersFootersMode.\nFiles saved at " + dataDir);
     }
 }
---
+++ Examples/src/main/java/com/aspose/words/examples/mail_merge/MailMergeCleanUp.java
@@ -0,0 +1,31 @@
+package com.aspose.words.examples.mail_merge;
+
+import com.aspose.words.MailMergeCleanupOptions;
+import com.aspose.words.examples.Utils;
+import com.aspose.words.Document;
+
+public class MailMergeCleanUp {
+    public static void main(String[] args) throws Exception {
+        String dataDir = Utils.getSharedDataDir(MailMergeCleanUp.class) + "MailMerge/";
+
+        cleanupParagraphsWithPunctuationMarks(dataDir);
+    }
+
+    public static void cleanupParagraphsWithPunctuationMarks(String dataDir) throws Exception {
+        // ExStart:CleanupParagraphsWithPunctuationMarks
+        // Open the document
+        Document doc = new Document(dataDir + "MailMerge.CleanupPunctuationMarks.docx");
+
+        doc.getMailMerge().setCleanupOptions(MailMergeCleanupOptions.REMOVE_EMPTY_PARAGRAPHS);
+        doc.getMailMerge().setCleanupParagraphsWithPunctuationMarks(false);
+
+        doc.getMailMerge().execute(new String[]{"field1", "field2"}, new Object[]{"", ""});
+
+        dataDir = dataDir + "MailMerge.CleanupPunctuationMarks_out.docx";
+        // Save the output document to disk.
+        doc.save(dataDir);
+        // ExEnd:CleanupParagraphsWithPunctuationMarks
+
+        System.out.println("\nMail merge performed with cleanup paragraphs having punctuation marks successfully.\nFile saved at " + dataDir);
+    }
+}
