c2253734028286a9aee5f3e4efc4a9b7d357c6eb
muzammil-khan <muzammil.khan@aspose.com>
2019-05-10 23:52:44 +0500

    Updates for Aspose.Words for Java 19.5 Release.

---
+++ Examples/src/main/java/Filed/FieldDisplayResult.java
@@ -0,0 +1,24 @@
+package Filed;
+
+import com.aspose.words.Document;
+import com.aspose.words.Field;
+import com.aspose.words.examples.Utils;
+import com.aspose.words.examples.programming_documents.joining_appending.AppendwithImportFormatOptions;
+
+public class FieldDisplayResult {
+
+	public static void main(String[] args) throws Exception {
+		// ExStart: FieldDisplayResult
+		// The path to the documents directory.
+		String dataDir = Utils.getDataDir(AppendwithImportFormatOptions.class);
+				
+		Document document = new Document(dataDir + "Document.docx");
+		document.updateFields();
+
+		for (Field field : document.getRange().getFields())
+		{
+		    System.out.println(field.getDisplayResult());
+		}
+		// ExEnd: FieldDisplayResult
+	}
+}
