2c98d039a64a1aaf75d2da68caa96b6322cc6b20
muzammil-khan <muzammil.khan@aspose.com>
2019-09-26 14:48:07 +0500

    Added example to use OpenTypeFetures

--- Examples/src/main/java/com/aspose/words/examples/programming_documents/document/OpenTypeFeatures.java
+++ Examples/src/main/java/com/aspose/words/examples/programming_documents/document/OpenTypeFeatures.java
@@ -2,11 +2,10 @@ package com.aspose.words.examples.programming_documents.document;
 
 import com.aspose.words.Document;
 import com.aspose.words.examples.Utils;
-import com.aspose.words.examples.programming_documents.LinkedTextboxes.WorkingWithLinkedTextboxes;
 
 public class OpenTypeFeatures {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		// ExStart:OpenTypeFeatures
         // The path to the documents directory.
@@ -17,7 +16,7 @@ public class OpenTypeFeatures {
 
         // When text shaper factory is set, layout starts to use OpenType features.
         // An Instance property returns static BasicTextShaperCache object wrapping HarfBuzzTextShaperFactory
-        doc.getLayoutOptions().setTextShaperFactory(com.aspose.words.Shaping.HarfBuzz.HarfBuzzTextShaperFactory.Instance);
+        doc.getLayoutOptions().setTextShaperFactory(com.aspose.words.shaping.harfbuzz.HarfBuzzTextShaperFactory.getInstance());
 
         // Render the document to PDF format
         doc.save(dataDir + "OpenType.Document.pdf");
