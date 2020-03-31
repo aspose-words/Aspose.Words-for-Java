7bfd46de78834b9c3fd07b69784acef3824ef50c
Muzammil Khan <muzammil.khan@aspose.com>
2020-03-31 17:15:04 +0500

    Update SetFontSettings.java

--- Examples/src/main/java/com/aspose/words/examples/rendering_printing/SetFontSettings.java
+++ Examples/src/main/java/com/aspose/words/examples/rendering_printing/SetFontSettings.java
@@ -1,7 +1,11 @@
 package com.aspose.words.examples.rendering_printing;
 
 import com.aspose.words.Document;
+import com.aspose.words.FolderFontSource;
 import com.aspose.words.FontSettings;
+import com.aspose.words.FontSourceBase;
+import com.aspose.words.LoadOptions;
+import com.aspose.words.SystemFontSource;
 import com.aspose.words.examples.Utils;
 
 public class SetFontSettings {
@@ -9,11 +13,24 @@ public class SetFontSettings {
         // The path to the documents directory.
         String dataDir = Utils.getSharedDataDir(SetFontSettings.class) + "RenderingAndPrinting/";
 
+        SetFontsFolder(dataDir);
         enableDisableFontSubstitution(dataDir);
         setFontFallbackSettings(dataDir);
         setPredefinedFontFallbackSettings(dataDir);
     }
 
+    public static void SetFontsFolder(String dataDir) throws Exception {
+		// ExStart:SetFontsFolder
+    	FontSettings.getDefaultInstance().setFontsSources(new FontSourceBase[] {
+    			    new SystemFontSource(), new FolderFontSource("C:\\MyFonts\\", true)
+    	});
+    	
+    	Document doc = new Document(dataDir + "Rendering.doc");
+    	dataDir = dataDir + "Rendering.SetFontsFolders_out.pdf";
+    	doc.save(dataDir);
+		// ExEnd:SetFontsFolder
+	}
+    
     public static void enableDisableFontSubstitution(String dataDir) throws Exception {
         // ExStart:EnableDisableFontSubstitution
         // The path to the documents directory.
