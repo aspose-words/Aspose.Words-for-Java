febd337319143bb71d1a532170eeb600ba66b0f0
Muzammil Khan <muzammil.khan@aspose.com>
2020-05-16 15:10:29 +0500

    Updates for Aspose.Words for Java 20.5

--- Examples/src/main/java/com/aspose/words/examples/programming_documents/styles/WorkingWithStyle.java
+++ Examples/src/main/java/com/aspose/words/examples/programming_documents/styles/WorkingWithStyle.java
@@ -4,41 +4,57 @@ import com.aspose.words.*;
 import com.aspose.words.examples.Utils;
 
 public class WorkingWithStyle {
-    public static void main(String[] args) throws Exception {
-        // The path to the documents directory.
-        String dataDir = Utils.getDataDir(WorkingWithStyle.class);
+	public static void main(String[] args) throws Exception {
+		// The path to the documents directory.
+		String dataDir = Utils.getDataDir(WorkingWithStyle.class);
 
-        cleansUnusedStylesandLists(dataDir);
-        copyStyles(dataDir);
-    }
+		cleansUnusedStylesandLists(dataDir);
+		copyStyles(dataDir);
+		CleanupDuplicateStyle(dataDir);
+	}
 
-    public static void cleansUnusedStylesandLists(String dataDir) throws Exception {
-        //ExStart:CleansUnusedStylesandLists
-        Document doc = new Document(dataDir + "TestFile.doc");
-        CleanupOptions cleanupoptions = new CleanupOptions();
+	public static void cleansUnusedStylesandLists(String dataDir) throws Exception {
+		// ExStart:CleansUnusedStylesandLists
+		Document doc = new Document(dataDir + "TestFile.doc");
+		CleanupOptions cleanupoptions = new CleanupOptions();
 
-        cleanupoptions.setUnusedLists(false);
-        cleanupoptions.setUnusedStyles(true);
+		cleanupoptions.setUnusedLists(false);
+		cleanupoptions.setUnusedStyles(true);
 
-        // Cleans unused styles and lists from the document depending on given CleanupOptions.
-        doc.cleanup(cleanupoptions);
-        doc.save(dataDir + "Document.Cleanup_out.docx");
-        //ExEnd:CleansUnusedStylesandLists
+		// Cleans unused styles and lists from the document depending on given
+		// CleanupOptions.
+		doc.cleanup(cleanupoptions);
+		doc.save(dataDir + "Document.Cleanup_out.docx");
+		// ExEnd:CleansUnusedStylesandLists
 
-        System.out.println("Document unused Styles cleaned successfully.");
-    }
+		System.out.println("Document unused Styles cleaned successfully.");
+	}
 
-    
-    public static void copyStyles(String dataDir) throws Exception {
-        // ExStart:CopyStylesFromDocument
-        Document doc = new Document(dataDir + "template.docx");
-        Document target = new Document(dataDir + "TestFile.doc");
+	public static void copyStyles(String dataDir) throws Exception {
+		// ExStart:CopyStylesFromDocument
+		Document doc = new Document(dataDir + "template.docx");
+		Document target = new Document(dataDir + "TestFile.doc");
 
-        target.copyStylesFromTemplate(doc);
+		target.copyStylesFromTemplate(doc);
 
-        dataDir = dataDir + "CopyStyles_out.docx";
-        doc.save(dataDir);
-        // ExEnd:CopyStylesFromDocument
-        System.out.println("\\nStyles are copied from document successfully.\\nFile saved at " + dataDir);
-    }
+		dataDir = dataDir + "CopyStyles_out.docx";
+		doc.save(dataDir);
+		// ExEnd:CopyStylesFromDocument
+		System.out.println("\\nStyles are copied from document successfully.\\nFile saved at " + dataDir);
+	}
+
+	private static void CleanupDuplicateStyle(String dataDir) throws Exception {
+		// ExStart:CleanupDuplicateStyle
+		Document doc = new Document(dataDir + "Document.doc");
+
+		CleanupOptions options = new CleanupOptions();
+		options.setDuplicateStyle(true);
+
+		// Cleans duplicate styles from the document.
+		doc.cleanup(options);
+
+		doc.save(dataDir + "Document.CleanupDuplicateStyle_out.docx");
+		// ExEnd:CleanupDuplicateStyle
+		System.out.println("\nAll revisions accepted.\nFile saved at " + dataDir);
+	}
 }
