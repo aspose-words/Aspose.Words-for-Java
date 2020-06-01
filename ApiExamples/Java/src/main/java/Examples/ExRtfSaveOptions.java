ea8c1430e39ba23bd1e605e39dccea6f18acdad0
sdfleod <sdfleod@gmail.com>
2020-06-01 16:21:40 +0700

    fix WORDSJAVA-2341
    I checked the test saveImagesAsWmf() with the last version of aspose.words.jdk17
    And I can not reproduce this bug.

--- ApiExamples/Java/src/main/java/Examples/ExRtfSaveOptions.java
+++ ApiExamples/Java/src/main/java/Examples/ExRtfSaveOptions.java
@@ -36,4 +36,29 @@ public class ExRtfSaveOptions extends ApiExampleBase {
         doc.save(getArtifactsDir() + "RtfSaveOptions.ExportImages.rtf", options);
         //ExEnd
     }
+
+    @Test
+    public void saveImagesAsWmf() throws Exception
+    {
+        //ExStart
+        //ExFor:RtfSaveOptions.SaveImagesAsWmf
+        //ExSummary:Shows how to save all images as Wmf when saving to the Rtf document.
+        // Open a document that contains images in the jpeg format
+        Document doc = new Document(getMyDir() + "Images.docx");
+
+        NodeCollection shapes = doc.getChildNodes(NodeType.SHAPE, true);
+        Shape shapeWithJpg = (Shape)shapes.get(0);
+        Assert.assertEquals(ImageType.JPEG, shapeWithJpg.getImageData().getImageType());
+
+        RtfSaveOptions rtfSaveOptions = new RtfSaveOptions();
+        rtfSaveOptions.setSaveImagesAsWmf(true);
+        doc.save(getArtifactsDir() + "RtfSaveOptions.SaveImagesAsWmf.rtf", rtfSaveOptions);
+
+        doc = new Document(getArtifactsDir() + "RtfSaveOptions.SaveImagesAsWmf.rtf");
+
+        shapes = doc.getChildNodes(NodeType.SHAPE, true);
+        Shape shapeWithWmf = (Shape)shapes.get(0);
+        Assert.assertEquals(ImageType.WMF, shapeWithWmf.getImageData().getImageType());
+        //ExEnd
+    }
 }
