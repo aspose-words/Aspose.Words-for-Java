e7b175857e6b0dec974a5695f3c4e2407f839e86
vderyushev <vyacheslav.deryushev@aspose.com>
2020-12-23 13:48:58 +0700

    Updated ExRange

--- ApiExamples/Java/src/main/java/Examples/ExPsSaveOptions.java
+++ ApiExamples/Java/src/main/java/Examples/ExPsSaveOptions.java
@@ -9,33 +9,52 @@ package Examples;
 //////////////////////////////////////////////////////////////////////////
 
 import com.aspose.words.*;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 @Test
-public class ExPsSaveOptions extends ApiExampleBase {
-    @Test
-    public void useBookFoldPrintingSettings() throws Exception {
+public class ExPsSaveOptions extends ApiExampleBase
+{
+    @Test (dataProvider = "useBookFoldPrintingSettingsDataProvider")
+    public void useBookFoldPrintingSettings(boolean renderTextAsBookFold) throws Exception
+    {
         //ExStart
         //ExFor:PsSaveOptions
         //ExFor:PsSaveOptions.SaveFormat
         //ExFor:PsSaveOptions.UseBookFoldPrintingSettings
-        //ExSummary:Shows how to create a bookfold in the PostScript format.
+        //ExSummary:Shows how to save a document to the Postscript format in the form of a book fold.
         Document doc = new Document(getMyDir() + "Paragraphs.docx");
 
-        // Configure both page setup and PsSaveOptions to create a book fold
-        for (Section s : doc.getSections()) {
-            s.getPageSetup().setMultiplePages(MultiplePagesType.BOOK_FOLD_PRINTING);
-        }
-
+        // Create a "PsSaveOptions" object that we can pass to the document's "Save" method
+        // to modify how that method converts the document to PostScript.
+        // Set the "UseBookFoldPrintingSettings" property to "true" to arrange the contents
+        // in the output Postscript document in a way that helps us make a booklet out of it.
+        // Set the "UseBookFoldPrintingSettings" property to "false" to save the document normally.
         PsSaveOptions saveOptions = new PsSaveOptions();
         {
             saveOptions.setSaveFormat(SaveFormat.PS);
-            saveOptions.setUseBookFoldPrintingSettings(true);
+            saveOptions.setUseBookFoldPrintingSettings(renderTextAsBookFold);
         }
 
-        // In order to make a booklet, we will need to print this document, stack the pages
-        // in the order they come out of the printer and then fold down the middle
+        // If we are rendering the document as a booklet, we must set the "MultiplePages"
+        // properties of all page setup objects of all sections to "MultiplePagesType.BookFoldPrinting".
+        for (Section s : doc.getSections())
+        {
+            s.getPageSetup().setMultiplePages(MultiplePagesType.BOOK_FOLD_PRINTING);
+        }
+
+        // Once we print this document on both sides of the pages, we can fold all the pages down the middle at once,
+        // and the contents will line up in a way that creates a booklet.
         doc.save(getArtifactsDir() + "PsSaveOptions.UseBookFoldPrintingSettings.ps", saveOptions);
         //ExEnd
     }
+
+	@DataProvider(name = "useBookFoldPrintingSettingsDataProvider")
+	public static Object[][] useBookFoldPrintingSettingsDataProvider() {
+		return new Object[][]
+		{
+			{false},
+			{true},
+		};
+	}
 }
