020d7dd275f739ba174281398998f5a60644b68b
Muzammil Khan <muzammil.khan@aspose.com>
2020-12-30 15:24:03 +0500

    Added Examples for TrackChanges

--- Examples/src/main/java/com/aspose/words/examples/programming_documents/document/TrackChanges.java
+++ Examples/src/main/java/com/aspose/words/examples/programming_documents/document/TrackChanges.java
@@ -1,5 +1,9 @@
 package com.aspose.words.examples.programming_documents.document;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+
 import com.aspose.words.*;
 import com.aspose.words.examples.Utils;
 
@@ -7,6 +11,9 @@ public class TrackChanges {
 	public static void main(String[] args) throws Exception {
 		String dataDir = Utils.getSharedDataDir(TrackChanges.class) + "Document/";
 
+		WorkWithTrackChanges(dataDir);
+		GenerateRevisionsWhenMovingNode(dataDir);
+		ApplyDifferentPropertiesWithRevisions(dataDir);
 		acceptRevisions(dataDir);
 		getRevisionTypes(dataDir);
 		getRevisionGroups(dataDir);
@@ -16,6 +23,162 @@ public class TrackChanges {
 		accessRevisedVersion(dataDir);
 	}
 
+	private static void WorkWithTrackChanges(String dataDir) throws Exception {
+		// ExStart:WorkWithTrackChanges
+		Document doc = new Document();
+		Body body = doc.getFirstSection().getBody();
+		Paragraph para = body.getFirstParagraph();
+
+		// Add text to the first paragraph, then add two more paragraphs.
+		para.appendChild(new Run(doc, "Paragraph 1. "));
+		body.appendParagraph("Paragraph 2. ");
+		body.appendParagraph("Paragraph 3. ");
+
+		// We have three paragraphs, none of which registered as any type of revision
+		// If we add/remove any content in the document while tracking revisions,
+		// they will be displayed as such in the document and can be accepted/rejected.
+		doc.startTrackRevisions("John Doe", new Date());
+
+		// This paragraph is a revision and will have the according "IsInsertRevision" flag set.
+		para = body.appendParagraph("Paragraph 4. ");
+		if(para.isInsertRevision())
+			System.out.println("isInsertRevision:" + para.isInsertRevision());
+
+		// Get the document's paragraph collection and remove a paragraph.
+		ParagraphCollection paragraphs = body.getParagraphs();
+		if(4 == paragraphs.getCount())
+			System.out.println("count:" + paragraphs.getCount());
+
+		para = paragraphs.get(2);
+		para.remove();
+
+		// Since we are tracking revisions, the paragraph still exists in the document, will have the "IsDeleteRevision" set
+		// and will be displayed as a revision in Microsoft Word, until we accept or reject all revisions.
+		if(4 == paragraphs.getCount())
+			System.out.println("count:" + paragraphs.getCount());
+
+		if(para.isDeleteRevision())
+			System.out.println("isDeleteRevision:" + para.isDeleteRevision());
+
+		// The delete revision paragraph is removed once we accept changes.
+		doc.acceptAllRevisions();
+		if(3 == paragraphs.getCount())
+			System.out.println("count:" + paragraphs.getCount());
+
+		// Stopping the tracking of revisions makes this text appear as normal text.
+		// Revisions are not counted when the document is changed.
+		doc.stopTrackRevisions();
+
+		// Save the document.
+		doc.save(dataDir + "Document.Revisions.docx");
+		// ExEnd:WorkWithTrackChanges
+	}
+	
+	private static void GenerateRevisionsWhenMovingNode(String dataDir) throws Exception {
+		// ExStart:GenerateRevisionsWhenMovingNode
+		// Generate document contents.
+		Document doc = new Document();
+		DocumentBuilder builder = new DocumentBuilder(doc);
+		builder.writeln("Paragraph 1");
+		builder.writeln("Paragraph 2");
+		builder.writeln("Paragraph 3");
+		builder.writeln("Paragraph 4");
+		builder.writeln("Paragraph 5");
+		builder.writeln("Paragraph 6");
+		Body body = doc.getFirstSection().getBody();
+		System.out.println("Paragraph count:" + body.getParagraphs().getCount());
+
+		// Start tracking revisions.
+		doc.startTrackRevisions("Author", new Date());
+
+		// Generate revisions when moving a node from one location to another.
+		Node node = body.getParagraphs().get(3);
+		Node endNode = body.getParagraphs().get(5).getNextSibling();
+		Node referenceNode = body.getParagraphs().get(0);
+		while (node != endNode)
+		{
+			Node nextNode = node.getNextSibling();
+			body.insertBefore(node, referenceNode);
+			node = nextNode;
+		}
+
+		// Stop the process of tracking revisions.
+		doc.stopTrackRevisions();
+
+		// There are 3 additional paragraphs in the move-from range.
+		System.out.println("Paragraph count: " + body.getParagraphs().getCount());
+		doc.save(dataDir + "out.docx");
+		// ExEnd:GenerateRevisionsWhenMovingNode
+	}
+
+	private static void ApplyDifferentPropertiesWithRevisions(String dataDir) throws Exception {
+		// ExStart:ApplyDifferentPropertiesWithRevisions
+		// Open a blank document.
+		Document doc = new Document();
+
+		// Insert an inline shape without tracking revisions.
+		Shape shape = new Shape(doc, ShapeType.CUBE);
+		shape.setWrapType(WrapType.INLINE);
+		shape.setWidth(100.0);
+		shape.setHeight(100.0);
+		doc.getFirstSection().getBody().getFirstParagraph().appendChild(shape);
+
+		// Start tracking revisions and then insert another shape.
+		doc.startTrackRevisions("John Doe");
+		shape = new Shape(doc, ShapeType.SUN);
+		shape.setWrapType(WrapType.INLINE);
+		shape.setWidth(100.0);
+		shape.setHeight(100.0);
+		doc.getFirstSection().getBody().getFirstParagraph().appendChild(shape);
+
+		// Get the document's shape collection which includes just the two shapes we added.
+		Node[] shapes = doc.getChildNodes(NodeType.SHAPE, true).toArray();
+		if(2 == shapes.length)
+			System.out.println("Shapes Count:" + shapes.length);
+
+		// Remove the first shape.
+		shapes[0].remove();
+
+		Shape sh = (Shape) shapes[0];
+		// Because we removed that shape while changes were being tracked, the shape counts as a delete revision.
+		if(ShapeType.CUBE == sh.getShapeType())
+			System.out.println("Shape is CUBE");
+		
+		if(sh.isDeleteRevision())
+			System.out.println("isDeleteRevision:" + sh.isDeleteRevision());
+
+		// And we inserted another shape while tracking changes, so that shape will count as an insert revision.
+		sh = (Shape) shapes[1];
+		if(ShapeType.SUN == sh.getShapeType())
+			System.out.println("Shape is SUN");
+		
+		if(sh.isInsertRevision())
+			System.out.println("IsInsertRevision:" + sh.isInsertRevision());
+
+		// The document has one shape that was moved, but shape move revisions will have two instances of that shape.
+		// One will be the shape at its arrival destination and the other will be the shape at its original location.
+		Node[] nc = doc.getChildNodes(NodeType.SHAPE, true).toArray();
+		if(2 == nc.length)
+			System.out.println("Shapes Count:" + nc.length);
+
+		Shape mvr = (Shape) nc[0];
+		// This is the move to revision, also the shape at its arrival destination.
+		if(mvr.isMoveFromRevision())
+			System.out.println("isMoveFromRevision:" + mvr.isMoveFromRevision());
+		
+		if(mvr.isMoveToRevision())
+			System.out.println("isMoveToRevision:" + mvr.isMoveToRevision());
+		
+		mvr = (Shape) nc[1];
+		// This is the move from revision, which is the shape at its original location.
+		if(mvr.isMoveFromRevision())
+			System.out.println("isMoveFromRevision:" + mvr.isMoveFromRevision());
+		
+		if(mvr.isMoveToRevision())
+			System.out.println("isMoveToRevision:" + mvr.isMoveToRevision());
+		// ExEnd:ApplyDifferentPropertiesWithRevisions
+	}
+	
 	private static void acceptRevisions(String dataDir) throws Exception {
 		// ExStart:AcceptAllRevisions
 		Document doc = new Document(dataDir + "Document.doc");
